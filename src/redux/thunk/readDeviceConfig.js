77df157ac60d4bf76b82c73fdf5d5dd4971c7449
Jeffrey Sloan <sloanjeffreyj@gmail.com>
2021-11-16 13:32:55 -0700

    Reverted to non-notification system

--- src/redux/thunk/readDeviceConfig.js
+++ src/redux/thunk/readDeviceConfig.js
@@ -1,6 +1,5 @@
 import {
   SERVICE_UUID,
-  CHARACTERISTIC_INFO_UUID,
   CHARACTERISTIC_OPERATION_UUID,
 } from '../../constants/microControllerUuid.js';
 import { setGroupConfig } from '../actions/setGroupConfig.js';
@@ -18,91 +17,88 @@ import {
 export const readDeviceConfig = () => {
   return (dispatch, getState, DeviceManager) => {
     const state = getState();
-    console.log('Reading device config...');
-    let characteristicObject =
-      state.bleManager.connectedDevice.monitorCharacteristicForService(
-        SERVICE_UUID,
-        CHARACTERISTIC_OPERATION_UUID,
-        (error, characteristic) => {
-          if (error) {
-            console.log('error');
-            return;
-          }
-          if (characteristic) {
-            let responseValue = charBase64.atob(characteristic.value);
-            let rawArray = responseValue.split(',');
-            // Position subtracted by 1 due to no operation being sent with data.
-            let newNickname = '';
-            if (rawArray[NICKNAME_POSITION - 1]) {
-              newNickname = rawArray[NICKNAME_POSITION - 1];
-            }
+    console.log('Reading device config');
+    for (let i = 0; i < MAX_GROUPS; i++) {
+      let dmxControllerResponse =
+        state.bleManager.connectedDevice.readCharacteristicForService(
+          SERVICE_UUID,
+          CHARACTERISTIC_OPERATION_UUID
+        );
+      dmxControllerResponse.then(() => {
+        console.log('dmxControllerResponse: ', dmxControllerResponse);
+        let responseValue = charBase64.atob(dmxControllerResponse._W.value);
+        console.log('reponseValue: ');
+        console.log(responseValue);
+        let rawArray = responseValue.split(',');
 
-            let newGroup = {
-              operation: 0,
-              id: rawArray[GROUP_POSITION - 1],
-              nickname: newNickname,
-              intensity: Number(rawArray[INTENSITY_POSITION - 1]),
-              circuits: [],
-            };
-
-            let newCircuits = [];
-            for (let j = 0; j < MAX_CHANNELS; j++) {
-              newCircuits[j] = Number(rawArray[CHANNEL_START_POSITION + j - 1]);
-            }
-            newGroup.circuits = newCircuits;
-            dispatch(setGroupConfig(newGroup));
-            console.log(
-              'Read value from CHARACTERISTIC_INFO_UUID: ',
-              newGroup.circuits
-            );
-          }
+        // Position subtracted by 1 due to no operation being sent with data.
+        let newNickname = '';
+        if (rawArray[NICKNAME_POSITION - 1]) {
+          newNickname = rawArray[NICKNAME_POSITION - 1];
         }
-      );
+
+        let newGroup = {
+          operation: 0,
+          id: rawArray[GROUP_POSITION - 1],
+          nickname: newNickname,
+          intensity: Number(rawArray[INTENSITY_POSITION - 1]),
+          circuits: [],
+        };
+
+        let newCircuits = [];
+        for (let j = 0; j < MAX_CHANNELS; j++) {
+          newCircuits[j] = Number(rawArray[CHANNEL_START_POSITION + j - 1]);
+        }
+        newGroup.circuits = newCircuits;
+        dispatch(setGroupConfig(newGroup));
+        // console.log("Read value from CHARACTERISTIC_INFO_UUID: ", newGroup.circuits);
+      });
+    }
   };
-};
 
-function testReadConfig(error, characteristic) {
-  console.log('Inside testReadConfig');
-}
-
-function readDeviceConfigAfterNotification(dispatch, state) {
-  try {
-    let dmxControllerResponse =
-      state.bleManager.connectedDevice.readCharacteristicForService(
-        SERVICE_UUID,
-        CHARACTERISTIC_INFO_UUID
-      );
-    dmxControllerResponse.then(() => {
-      console.log('dmxControllerResponse: ', dmxControllerResponse);
-      let responseValue = charBase64.atob(dmxControllerResponse._W.value);
-      console.log('reponseValue: ');
-      console.log(responseValue);
-      let rawArray = responseValue.split(',');
-
-      // Position subtracted by 1 due to no operation being sent with data.
-      let newNickname = '';
-      if (rawArray[NICKNAME_POSITION - 1]) {
-        newNickname = rawArray[NICKNAME_POSITION - 1];
-      }
-
-      let newGroup = {
-        operation: 0,
-        id: rawArray[GROUP_POSITION - 1],
-        nickname: newNickname,
-        intensity: Number(rawArray[INTENSITY_POSITION - 1]),
-        circuits: [],
-      };
-
-      let newCircuits = [];
-      for (let j = 0; j < MAX_CHANNELS; j++) {
-        newCircuits[j] = Number(rawArray[CHANNEL_START_POSITION + j - 1]);
-      }
-      newGroup.circuits = newCircuits;
-      dispatch(setGroupConfig(newGroup));
-      // console.log("Read value from CHARACTERISTIC_INFO_UUID: ", newGroup.circuits);
-    });
-  } catch (error) {
-    console.log('readDeviceConfig Error: ', error);
-    return false;
+  function testReadConfig(error, characteristic) {
+    console.log('Inside testReadConfig');
   }
-}
+
+  function readDeviceConfigAfterNotification(dispatch, state) {
+    try {
+      let dmxControllerResponse =
+        state.bleManager.connectedDevice.readCharacteristicForService(
+          SERVICE_UUID,
+          CHARACTERISTIC_OPERATION_UUID
+        );
+      dmxControllerResponse.then(() => {
+        console.log('dmxControllerResponse: ', dmxControllerResponse);
+        let responseValue = charBase64.atob(dmxControllerResponse._W.value);
+        console.log('reponseValue: ');
+        console.log(responseValue);
+        let rawArray = responseValue.split(',');
+
+        // Position subtracted by 1 due to no operation being sent with data.
+        let newNickname = '';
+        if (rawArray[NICKNAME_POSITION - 1]) {
+          newNickname = rawArray[NICKNAME_POSITION - 1];
+        }
+
+        let newGroup = {
+          operation: 0,
+          id: rawArray[GROUP_POSITION - 1],
+          nickname: newNickname,
+          intensity: Number(rawArray[INTENSITY_POSITION - 1]),
+          circuits: [],
+        };
+
+        let newCircuits = [];
+        for (let j = 0; j < MAX_CHANNELS; j++) {
+          newCircuits[j] = Number(rawArray[CHANNEL_START_POSITION + j - 1]);
+        }
+        newGroup.circuits = newCircuits;
+        dispatch(setGroupConfig(newGroup));
+        // console.log("Read value from CHARACTERISTIC_INFO_UUID: ", newGroup.circuits);
+      });
+    } catch (error) {
+      console.log('readDeviceConfig Error: ', error);
+      return false;
+    }
+  }
+};
